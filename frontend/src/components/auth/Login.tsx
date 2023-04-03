f9cc180a94264fd576accd83fd95bd1fb4bff4f4
Justin Cheng <justinhimercheng01@gmail.com>
2023-04-03 20:59:53 +0800

    added sign up, not completed

--- frontend/src/components/auth/Login.tsx
+++ frontend/src/components/auth/Login.tsx
@@ -1,11 +1,8 @@
 import { IonLabel, IonSegment, IonSegmentButton } from '@ionic/react';
 import { useState } from 'react';
 import UserStyle from '../../scss/User.module.scss';
-import { CornerBox } from '../CornerBox';
 import { Logo } from '../Logo';
 import { UserLoginForm } from './UserLoginForm';
-import { FacebookLogin } from './FacebookLogin';
-import { Logout } from './Logout';
 import { GymLoginForm } from './GymLoginForm';
 
 export function Login() {
