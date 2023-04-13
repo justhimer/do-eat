cf7e29511c4dbc3d135959e17b88c0d8f9dfe40b
Geoffrey Luk <luk.geoffreycw@gmail.com>
2023-04-13 17:29:02 +0800

    gym cms backend complete

--- backend/src/food_history/food_history.controller.ts
+++ backend/src/food_history/food_history.controller.ts
@@ -1,9 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request, ParseIntPipe, BadRequestException } from '@nestjs/common';
 import { FoodHistoryService } from './food_history.service';
 import { CreateFoodHistoryDto } from './dto/create-food_history.dto';
 import { UpdateFoodHistoryDto } from './dto/update-food_history.dto';
 import { AuthGuard } from '@nestjs/passport';
+import { ApiTags } from '@nestjs/swagger';
+import { isError } from 'lodash';
 
+@ApiTags('food-history')
 @Controller('food-history')
 export class FoodHistoryController {
   constructor(private readonly foodHistoryService: FoodHistoryService) {}
@@ -21,18 +24,33 @@ export class FoodHistoryController {
     return this.foodHistoryService.create(createFoodHistoryDto);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.foodHistoryService.findOne(+id);
+  @Get('gym/:gym_id')
+  async findOne(@Param('gym_id', ParseIntPipe) gym_id: number) {
+    const data = await this.foodHistoryService.findOrdersForGyms(gym_id);
+    if (!isError(data)){
+      const newData = []
+      data.forEach(order=>{
+        const foodsInOrder = []
+        order.FoodOrder.forEach(food=>{
+          foodsInOrder.push({
+            food_id:food.id,
+            quantity:food.quantity,
+            name:food.food.name,
+            image:food.food.image
+          })
+        })
+        newData.push({
+          order_id:order.id,
+          user_id:order.user_id,
+          user: order.user.username,
+          collection_status:order.collection_status,
+          foodOrder:foodsInOrder
+        })
+      })
+      return newData
+    }else{
+      throw new BadRequestException('Bad Request', { cause: new Error() })
+    }
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateFoodHistoryDto: UpdateFoodHistoryDto) {
-    return this.foodHistoryService.update(+id, updateFoodHistoryDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.foodHistoryService.remove(+id);
-  }
 }
