b46f310e0ca74dba56820d53ef235a8d8651f018
Jayden Choi <henrychoi1997@hotmail.com>
2023-04-15 19:32:40 +0800

    mutation to other table

--- frontend/src/components/ShoppingCart/cart.tsx
+++ frontend/src/components/ShoppingCart/cart.tsx
@@ -1,5 +1,7 @@
-import { IonPage, IonHeader, IonToolbar, IonButton, IonBackButton, IonTitle, IonContent, IonList, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from "@ionic/react";
+import { IonPage, IonHeader, IonToolbar, IonButton, IonBackButton, IonTitle, IonContent, IonList, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonSelect, IonSelectOption } from "@ionic/react";
 import { useMutation, useQuery } from "@tanstack/react-query";
+import { cleanup } from "@testing-library/react";
+import { clear } from "console";
 import { useState } from "react";
 import { fetchAllCartItems } from "../../api/cartAPI";
 import { UserOrderItem } from "../user/UserOrderItem";
@@ -22,9 +24,13 @@ export function ShoppingCart() {
 
     function onCheckout() {
 
+
+    }
+    function onSave() {
+
     }
 
-    function onSave() {
+    function cancel() {
 
     }
 
@@ -60,9 +66,17 @@ export function ShoppingCart() {
                             }
 
                         </IonList>
-
+                        <IonSelect aria-label="districts" placeholder="Select districts">
+                            <IonSelectOption value="Pure">PureFitness CausewayBay</IonSelectOption>
+                            <IonSelectOption value="Pure">PureFitness Central</IonSelectOption>
+                            <IonSelectOption value="Pure">PureFitness Wan Chai</IonSelectOption>
+                            <IonSelectOption value="Pure">PureFitness Tsim Sha Tsui</IonSelectOption>
+                            <IonSelectOption value="Pure">PureFitness Mong Kok</IonSelectOption>
+                            <IonSelectOption value="247">247Fitness CausewayBay</IonSelectOption>
+                        </IonSelect>
                         <IonButton onClick={onSave}>Save Cart</IonButton>
                         <IonButton onClick={onCheckout}>Checkout</IonButton>
+                        <IonButton onClick={cancel}>Cancel</IonButton>
 
                     </IonCardContent>
                 </IonCard>
