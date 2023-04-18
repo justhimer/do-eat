ced1e1e92f41281a2c7d06817eb6990c110838be
Jayden Choi <henrychoi1997@hotmail.com>
2023-04-18 11:00:53 +0800

    delete button bug fixed

--- frontend/src/components/ShoppingCart/cartItem.tsx
+++ frontend/src/components/ShoppingCart/cartItem.tsx
@@ -1,7 +1,7 @@
 import { IonItem, IonThumbnail, IonLabel, IonButton } from "@ionic/react";
-import { image } from "ionicons/icons";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { useState } from "react";
-
+import { deleteCart } from "../../api/cartAPI";
 
 // css
 import UserMenuStyle from '../../scss/UserMenu.module.scss';
@@ -11,14 +11,28 @@ export interface CartItemProps {
     name: string;
     quantity: number;
     calories: number;
-    changeQuantity: (id: number, newQuantity: number) => void;
-    refetch: () => void;
     image: string;
+    changeQuantity: (id: number, newQuantity: number) => void;
+    refetch: () => void
+    // image: string;
 }
 
 export function CartItem(props: CartItemProps) {
 
     const [newQuantity, setNewQuantity] = useState(props.quantity);
+    const [counts, setCounts] = useState(0)
+
+    const deletingFood = useMutation(() => deleteCart(props.id), {
+        onSuccess: (data) => {
+            console.log('success')
+            props.refetch()
+
+        },
+        onError: (error) => {
+            console.log('fail')
+        }
+    })
+
 
     function onClickPlus() {
         props.changeQuantity(props.id, newQuantity + 1);
@@ -31,15 +45,18 @@ export function CartItem(props: CartItemProps) {
         setNewQuantity(newQuantity - 1);
     }
 
+
+
     return (<>
         <IonItem className={UserMenuStyle.item_last}>
             <IonThumbnail slot="start">
-                <img alt="Silhouette of mountains" src={`./assets/foodimage/${props.image}`} />
+                <img alt="food icon" src={`./assets/foodimage/${props.image}`} />
             </IonThumbnail>
             <p className={UserMenuStyle.width_70}>
                 {props.name}
                 <br />
                 Calories: {props.calories}
+                <IonButton onClick={() => deletingFood.mutate()}>x</IonButton>
             </p>
             <IonButton onClick={onClickMinus}>-</IonButton>
             <p>{newQuantity}</p>
